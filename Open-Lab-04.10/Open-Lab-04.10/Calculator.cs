1b797a572c9657c6586b67360f9746391421eb87
Ondrejmuran4691 <58992354+Ondrejmuran4691@users.noreply.github.com>
2021-11-09 22:14:28 +0100

    returns average from numbers in array

---
+++ Open-Lab-04.10/Open-Lab-04.10/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Open_Lab_04._10
+{
+    public class Calculator
+    {
+        public float Average(int[] nums)
+        {
+            float celkom = 0;
+            float counter = 0;
+            foreach (float i in nums)
+            {
+                celkom = celkom + i;
+                counter++;
+            }
+            
+            return celkom/counter;
+        }
+    }
+}
